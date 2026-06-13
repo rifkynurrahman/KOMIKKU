a3ff886558fb646e5e07d28eb0392c0ca6d378da
unknown <hernanda.habib2gmail.com>
2026-06-14 02:19:27 +0700

    coba debug

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,9 +1,9 @@
 // middleware/isAdmin.js
 module.exports = (req, res, next) => {
+    console.log("User saat ini:", req.session.user); // <--- Tambahkan ini untuk cek di terminal VS Code
     if (req.session.user && req.session.user.role === 'admin') {
         next();
     } else {
-        // Jika bukan admin, tendang ke beranda dengan pesan error
         res.status(403).send('Akses Ditolak: Halaman ini khusus Admin.');
     }
 };
